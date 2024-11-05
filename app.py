0d1e4acccb722d20733dbea7a9f85561037eb82a
AforAmateur <50123369+AforAmateur@users.noreply.github.com>
2024-11-05 16:56:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 import streamlit as st
-# from PIL import Image
 
-# st.logo(
-#     "https://dscvryai.com/assets/images/logos/logo-nobg-darktext.webp",
-#     link="https://dscvryai.com/",
-#     icon_image="https://dscvryai.com/assets/images/logos/favicon.ico",
-# )
-
-
-# Load favicon and set page config
-# im = Image.open("favicon.ico")
-st.set_page_config(
-    # page_icon=im,
+st.set_page_config(    
     layout="wide",
     page_title="Test App"
 )
